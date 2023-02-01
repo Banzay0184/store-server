0e5f0c22d192a02fd4cd77729842041a29b5a720
Banzay0184 <89055626+Banzay0184@users.noreply.github.com>
2023-02-02 01:48:03 +0500

    3

--- store/products/views.py
+++ store/products/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    context = {
+        'title': 'Store',
+    }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    context = {
+        'title': 'Store - Каталог',
+        'products': [
+            {
+                'imag': '/static/vendor/img/products/Adidas-hoodie.png',
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 6090,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+
+            },
+        ]
+    }
+    return render(request, 'products/products.html', context)
